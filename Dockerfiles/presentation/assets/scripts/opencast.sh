7609946f42d3bb3b3437b50f47309ad80f771dbf
Kristof Keppens <kkeppens@gmail.com>
2019-05-08 13:08:57 +0200

    enable the streaming config settings

--- Dockerfiles/presentation/assets/scripts/opencast.sh
+++ Dockerfiles/presentation/assets/scripts/opencast.sh
@@ -19,6 +19,7 @@ set -e
 export ORG_OPENCASTPROJECT_SERVER_URL="${ORG_OPENCASTPROJECT_SERVER_URL:-http://$(hostname -f):8080}"
 export ORG_OPENCASTPROJECT_ADMIN_EMAIL="${ORG_OPENCASTPROJECT_ADMIN_EMAIL:-admin@localhost}"
 export ORG_OPENCASTPROJECT_DOWNLOAD_URL="${ORG_OPENCASTPROJECT_DOWNLOAD_URL:-\$\{org.opencastproject.server.url\}/static}"
+export PROP_ORG_OPENCASTPROJECT_PLAYER="${PROP_ORG_OPENCASTPROJECT_PLAYER:-/paella/ui/watch.html}"
 
 if opencast_helper_dist_allinone || opencast_helper_dist_develop; then
   # shellcheck disable=SC2016
@@ -45,7 +46,9 @@ opencast_opencast_check() {
     "ORG_OPENCASTPROJECT_SECURITY_DIGEST_PASS" \
     "PROP_ORG_OPENCASTPROJECT_FILE_REPO_URL" \
     "PROP_ORG_OPENCASTPROJECT_ADMIN_UI_URL" \
-    "PROP_ORG_OPENCASTPROJECT_ENGAGE_UI_URL"
+    "PROP_ORG_OPENCASTPROJECT_ENGAGE_UI_URL" \
+    "ORG_OPENCASTPROJECT_STREAMING_URL" \
+    "ORG_OPENCASTPROJECT_ADAPTIVE_STREAMING_URL"
 
   if opencast_helper_dist_migration ; then
     opencast_helper_checkforvariables "ORG_OPENCASTPROJECT_MIGRATION_ORGANIZATION"
@@ -54,21 +57,24 @@ opencast_opencast_check() {
 
 opencast_opencast_configure() {
   echo "Run opencast_opencast_configure"
-  # opencast_helper_replaceinfile "${OPENCAST_CONFIG}/custom.properties" \
-  #   "ORG_OPENCASTPROJECT_ADMIN_EMAIL" \
-  #   "ORG_OPENCASTPROJECT_SERVER_URL" \
-  #   "ORG_OPENCASTPROJECT_SECURITY_ADMIN_USER" \
-  #   "ORG_OPENCASTPROJECT_SECURITY_ADMIN_PASS" \
-  #   "ORG_OPENCASTPROJECT_SECURITY_DIGEST_USER" \
-  #   "ORG_OPENCASTPROJECT_SECURITY_DIGEST_PASS" \
-  #   "ORG_OPENCASTPROJECT_DOWNLOAD_URL"
+  opencast_helper_replaceinfile "${OPENCAST_CONFIG}/custom.properties" \
+    "ORG_OPENCASTPROJECT_ADMIN_EMAIL" \
+    "ORG_OPENCASTPROJECT_SERVER_URL" \
+    "ORG_OPENCASTPROJECT_SECURITY_ADMIN_USER" \
+    "ORG_OPENCASTPROJECT_SECURITY_ADMIN_PASS" \
+    "ORG_OPENCASTPROJECT_SECURITY_DIGEST_USER" \
+    "ORG_OPENCASTPROJECT_SECURITY_DIGEST_PASS" \
+    "ORG_OPENCASTPROJECT_DOWNLOAD_URL" \
+    "ORG_OPENCASTPROJECT_STREAMING_URL" \
+    "ORG_OPENCASTPROJECT_ADAPTIVE_STREAMING_URL"
 
-  # # opencast_helper_replaceinfile "${OPENCAST_CONFIG}/org.opencastproject.organization-mh_default_org.cfg" \
-  # #   "PROP_ORG_OPENCASTPROJECT_FILE_REPO_URL" \
-  # #   "PROP_ORG_OPENCASTPROJECT_ADMIN_UI_URL" \
-  # #   "PROP_ORG_OPENCASTPROJECT_ENGAGE_UI_URL"
+  opencast_helper_replaceinfile "${OPENCAST_CONFIG}/org.opencastproject.organization-mh_default_org.cfg" \
+    "PROP_ORG_OPENCASTPROJECT_FILE_REPO_URL" \
+    "PROP_ORG_OPENCASTPROJECT_ADMIN_UI_URL" \
+    "PROP_ORG_OPENCASTPROJECT_ENGAGE_UI_URL" \
+    "PROP_ORG_OPENCASTPROJECT_PLAYER"
 
-  # if opencast_helper_dist_migration ; then
-  #   opencast_helper_replaceinfile "${OPENCAST_CONFIG}/custom.properties" "ORG_OPENCASTPROJECT_MIGRATION_ORGANIZATION"
-  # fi
+  if opencast_helper_dist_migration ; then
+    opencast_helper_replaceinfile "${OPENCAST_CONFIG}/custom.properties" "ORG_OPENCASTPROJECT_MIGRATION_ORGANIZATION"
+  fi
 }
