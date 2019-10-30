ab9f6b0492474bb314c15af4ec9b04fa691291e1
Kristof Keppens <kkeppens@gmail.com>
2019-10-30 14:48:17 +0100

    remove org config

--- Dockerfiles/allinone/assets/scripts/opencast.sh
+++ Dockerfiles/allinone/assets/scripts/opencast.sh
@@ -65,11 +65,6 @@ opencast_opencast_configure() {
     "ORG_OPENCASTPROJECT_STREAMING_URL" \
     "ORG_OPENCASTPROJECT_ADAPTIVE_STREAMING_URL" 
 
-  opencast_helper_replaceinfile "etc/org.opencastproject.organization-mh_default_org.cfg" \
-    "PROP_ORG_OPENCASTPROJECT_FILE_REPO_URL" \
-    "PROP_ORG_OPENCASTPROJECT_ADMIN_UI_URL" \
-    "PROP_ORG_OPENCASTPROJECT_ENGAGE_UI_URL"
-
   if opencast_helper_dist_migration ; then
     opencast_helper_replaceinfile "etc/custom.properties" "ORG_OPENCASTPROJECT_MIGRATION_ORGANIZATION"
   fi
